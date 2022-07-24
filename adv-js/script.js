1d7e96c25dfb23d14f9b820fc7d33f3c1801a9ed
themarcBlanchard <marcblanchard82@gmail.com>
2022-07-24 19:55:12 -0400

    Finished the to do list. User can either press Enter, or press the button

--- adv-js/script.js
+++ adv-js/script.js
@@ -9,13 +9,31 @@ const input = document.getElementById('user_input');
 const ul = document.querySelector('ul');
 
 
-// dom event listeners
-btn.addEventListener('click', function() {
-    if(input.value.length > 0) {
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = '';
+// functions
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = '';
+}
+
+function addListAfterClick(){
+    if(inputLength() > 0) {
+        createListElement();
+     }
+}
+
+function addListAfterEnter(event){
+    if(inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
     }
-});
+}
+
+// dom event listeners
+btn.addEventListener('click', addListAfterClick);
+input.addEventListener('keypress', addListAfterEnter);
 
